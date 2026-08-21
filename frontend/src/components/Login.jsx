d509603e2f54ae5282a8652da9ab134a888c8a0d
Mohit-Hans <mohitqwe582@gmail.com>
2026-08-21 15:41:28 +0530

    add login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from 'react';
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <div className="container">
+        <div className="form-box login">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <h1>Login</h1>
+            <div className="input-box">
+              <input type="text" placeholder="Username" required />
+              <i className='bx bxs-user'></i>
+            </div>
+            <div className="input-box">
+              <input type="password" placeholder="Password" required />
+              <i className='bx bxs-lock-alt'></i>
+            </div>
+            <div className="forgot-link">
+              <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit" className="btn">Login</button>
+            <p>or login with social platforms</p>
+            <div className="social-icons">
+              <a href="#"><i className='bx bxl-google'></i></a>
+              <a href="#"><i className='bx bxl-facebook'></i></a>
+              <a href="#"><i className='bx bxl-github'></i></a>
+              <a href="#"><i className='bx bxl-linkedin'></i></a>
+            </div>
+          </form>
+        </div>
+
+        <div className="form-box register">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <h1>Registration</h1>
+            <div className="input-box">
+              <input type="text" placeholder="Username" required />
+              <i className='bx bxs-user'></i>
+            </div>
+            <div className="input-box">
+              <input type="email" placeholder="Email" required />
+              <i className='bx bxs-envelope'></i>
+            </div>
+            <div className="input-box">
+              <input type="password" placeholder="Password" required />
+              <i className='bx bxs-lock-alt'></i>
+            </div>
+            <button type="submit" className="btn">Register</button>
+            <p>or register with social platforms</p>
+            <div className="social-icons">
+              <a href="#"><i className='bx bxl-google'></i></a>
+              <a href="#"><i className='bx bxl-facebook'></i></a>
+              <a href="#"><i className='bx bxl-github'></i></a>
+              <a href="#"><i className='bx bxl-linkedin'></i></a>
+            </div>
+          </form>
+        </div>
+
+        <div className="toggle-box">
+          <div className="toggle-panel toggle-left">
+            <h1>Hello, Welcome!</h1>
+            <p>Don't have an account?</p>
+            <button className="btn register-btn">Register</button>
+          </div>
+
+          <div className="toggle-panel toggle-right">
+            <h1>Welcome Back!</h1>
+            <p>Already have an account?</p>
+            <button className="btn login-btn">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
