14af185b7f125fd296d05df2e81d06ee1b42d35c
Dheerajkumar <dheerajkumar9877k@gmail.com>
2026-08-22 08:50:10 +0530

    Add createuser and login in models

---
+++ backend/controller/authController.js
@@ -0,0 +1,6 @@
+const db = require('../config/db')
+const UserModels = require('../models/authModel');
+
+class UserController{
+    
+}
--- backend/models/authModel.js
+++ backend/models/authModel.js
@@ -1,30 +1,55 @@
-const db = require('../config/db');
-
-class UserModels {
-    static createUser(name , email ,password , callback) {
-        const mysql = "INSERT INTO expense_tracker(name , email, password) VALUES (?) ";
-        const value = [
-            name,
-            email,
-            password
+const bcrypt = require('bcrypt');
+const db = require('../config/db')
+class UserModels  {
+    // Create User
+    async createUser(name , email ,password ) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const sql ="INSERT INTO login(name , email , password) VALUES(?,?,?)";
+        const values = [
+            name ,
+            email ,
+            hashedPassword ,
         ]
-        db.query(sql,value,(err,result) => {
+        db.query(sql,[values],(err,result) => {
             if(err){
-                return callback(err , null);
+                console.log(err);
+                return;
             }
-            callback(null, result);
+            console.log("Created Sussfully");
+            console.log(result);
         });
     }
-    static findUser(email,password,callback){
-        const mysql = "SELECT * FROM expense_tracker WHERE email = ?" ;
-        db.query(sql , [email] , (err,result) =>{
-            if (err) {
-                return callback(err, null);
-            }
+    // login User
+   async login(email ,password) {
 
-            callback(null, result);
-        })
-    }
+    const sql = "SELECT * FROM login WHERE email = ?"
+
+    db.query(sql, [email], async(err,result) =>{
+
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        if(result.length===0){
+            console.log("Invalid email pr Password");
+            return ;
+        }
+
+        const user = result[0] ;
+
+        const isMatch = await bcrypt.compare(
+            password ,
+            user.password
+        );
+
+        if(!isMatch){
+            console.log("Invalid Email or Password");
+            return;
+        }
+
+        console.log("Logined");
+    });
+   }
 }
-
-module.exports = UserModels;
+module.exports = UserModels ;
